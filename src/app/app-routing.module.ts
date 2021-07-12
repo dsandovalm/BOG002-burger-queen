42fe29fb2ad200fcdb2de82c6b569b1232fa4b6b
DianaCato <diana.catolico@hotmail.com>
2021-07-12 13:58:09 -0500

    templates and style initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CocinaComponent } from './templates/cocina/cocina.component';
+import { InicioComponent } from './templates/inicio/inicio.component';
+import { MesaComponent } from './templates/mesa/mesa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path:'mesa', component: MesaComponent},
+  {path:'cocina', component:CocinaComponent},
+  {path:'**', pathMatch:'full', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './templates/inicio/inicio.component';
+import { MesaComponent } from './templates/mesa/mesa.component';
+import { CocinaComponent } from './templates/cocina/cocina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InicioComponent,
+    MesaComponent,
+    CocinaComponent,
   ],
   imports: [
     BrowserModule,
