eb4514eebd343fc5ee14dd85381ebbc41fe5e462
Julieth <julieth@MacBook-Pro-de-Julieth.local>
2021-07-12 14:44:44 -0500

    Router y componentes principales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CocinaComponent } from './cocina/cocina.component';
+import { HomeComponent } from './home/home.component';
+import { MesaComponent } from './mesa/mesa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'mesa', component: MesaComponent },
+  { path: 'cocina', component: CocinaComponent },
+  { path: '**', pathMatch:'full', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  constructor(){
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
+import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MesaComponent } from './mesa/mesa.component';
+import { CocinaComponent } from './cocina/cocina.component';
+
+const routes : Routes = [
+  { path: ' ', component: HeaderComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    MesaComponent,
+    CocinaComponent
   ],
   imports: [
     BrowserModule,
