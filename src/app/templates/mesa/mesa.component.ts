5d1922cb38afa39704db3582836d510bae471492
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-07-14 13:05:22 -0500

    Add files via upload

--- src/app/templates/mesa/mesa.component.ts
+++ src/app/templates/mesa/mesa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import menuData from 'src/app/services/menu.json'
 
 @Component({
   selector: 'app-mesa',
@@ -6,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mesa.component.scss']
 })
 export class MesaComponent implements OnInit {
+  menu:any = menuData;
+  tipo:string = 'principal';
 
   btnNewOrder:string = 'NUEVO PEDIDO';
   btnEditOrder:string = 'EDITAR PEDIDO';
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  getActivo(): any{
+    return this.menu[this.tipo]
+  }
+
+  switchMenu(): void{
+    this.tipo = this.tipo === 'principal' ? this.tipo = 'desayunos' : this.tipo = 'principal';
   }
 
 }
