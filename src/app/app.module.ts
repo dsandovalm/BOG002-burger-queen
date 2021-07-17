b2088c89157855b645655adb6112db2d479d639b
DianaCato <diana.catolico@hotmail.com>
2021-07-17 10:59:36 -0500

    merge themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-=======
 import { ReactiveFormsModule } from '@angular/forms';
->>>>>>> develop
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './templates/inicio/inicio.component';
 import { MesaComponent } from './templates/mesa/mesa.component';
 import { CocinaComponent } from './templates/cocina/cocina.component';
-<<<<<<< HEAD
-import { ButtonPrimaryComponent } from './components/button-primary/button-primary.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MenuComponent } from './components/menu/menu.component';
-=======
-<<<<<<< HEAD
 import { OrderComponent } from './components/order/order.component';
-import { ButtonPrimaryComponent } from './components/button-primary/button-primary.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MenuComponent } from './components/menu/menu.component';
-=======
 import { ButtonPrimaryComponent } from './components/button-primary/button-primary.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -30,8 +17,6 @@ import { PopUpComponent } from './components/pop-up/pop-up.component';
 import { InicialComponent } from './components/estados-mesa/inicial/inicial.component';
 import { OrdenComponent } from './components/estados-mesa/orden/orden.component';
 import { ComandaComponent } from './components/comanda/comanda.component';
->>>>>>> develop
->>>>>>> 7f4ba275063d827c375e3c45f403f04be4191b32
 
 @NgModule({
   declarations: [
@@ -39,19 +24,7 @@ import { ComandaComponent } from './components/comanda/comanda.component';
     InicioComponent,
     MesaComponent,
     CocinaComponent,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
     OrderComponent,
->>>>>>> 7f4ba275063d827c375e3c45f403f04be4191b32
-    ButtonPrimaryComponent,
-    HeaderComponent,
-    MenuComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-=======
     ButtonPrimaryComponent,
     HeaderComponent,
     MenuComponent,
@@ -66,7 +39,6 @@ import { ComandaComponent } from './components/comanda/comanda.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule
->>>>>>> develop
   ],
   providers: [],
   bootstrap: [AppComponent]
