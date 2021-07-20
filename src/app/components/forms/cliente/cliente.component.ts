8f9ff7cac2517b541a8dfba3f8aed8a677ad7dd8
DianaCato <diana.catolico@hotmail.com>
2021-07-19 20:04:05 -0500

    merge feature-cliente

--- src/app/components/forms/cliente/cliente.component.ts
+++ src/app/components/forms/cliente/cliente.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { PopUpsService } from 'src/app/services/pop-ups.service';
 import { EstadosMesaService } from 'src/app/services/estados-mesa.service';
