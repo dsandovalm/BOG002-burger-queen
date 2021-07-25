b2ae2427a854a2e3868fdc9a2614f1729925e797
Diana Sandoval <dsandovalm@unal.edu.co>
2021-07-25 02:43:20 -0500

    Arreglado bug de Firebase. Ahora actualiza la info a guardar antes de hacerlo

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { itemInterface } from '../model/item.model';
 import { pedidoInterface } from '../model/pedido.model';
 import { ClienteService } from './cliente.service';
 import { PedidoService } from './pedido.service';
@@ -25,10 +26,17 @@ export class FirestoreService {
     private firestore: AngularFirestore,
     private clienteService:ClienteService,
     private pedidoService: PedidoService
-    ) { }
+    ) {
+      this.pedidoService.addItem.subscribe((items:itemInterface[])=>{
+        this.pedido.items = items;
+      })
+    }
+
 
   /* Almacenar pedidos */
   public setOrder() {
+    this.pedido.cliente.nombre = this.clienteService.get().nombre;
+    this.pedido.cliente.mesa = this.clienteService.get().mesa;
     return this.firestore.collection('pedidos').add(this.pedido);
   }
 
